c68965a3b61af5efa24fc552ae6551adaad2143d
randilfernando <randil.fernando@outlook.com>
2024-03-23 06:17:53 +0530

    [TASK-86enx3rqk] get origin url

--- github/src/index.ts
+++ github/src/index.ts
@@ -1,11 +1,14 @@
 import {CPMContext, ActionInput, ActionOutput, CPMPlugin, Action} from '@cloudimpl-inc/cpm';
+import {executeShellCommand} from "@cloudimpl-inc/cpm/dist/util";
 
 const gitPlugin: CPMPlugin = {
     name: 'git',
     actions: {
         'pr create': async (ctx: CPMContext, input: ActionInput): Promise<ActionOutput> => {
             const { head, base } = input.args;
-            const url = `https://github.com/CloudImpl-Inc/beeplk/compare/${base}...${head}`
+            let {output: originUrl} = await executeShellCommand('git config --get remote.origin.url');
+            originUrl = originUrl.split('\n')[0];
+            const url = `${originUrl}/compare/${base}...${head}`
             console.log(`Please visit this URL to create pull request: ${url}`);
             return {};
         },
